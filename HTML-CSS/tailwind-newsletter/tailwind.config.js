7eecae963f40d11a893bea1f5ec8df5f80d2f82a
Estevão Henrique <estevao@vibecriativa.com.br>
2022-05-19 14:17:53 -0300

    ajustes

---
+++ HTML-CSS/tailwind-newsletter/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    content: [
+        'newsletter.{html}',
+      ],
+  theme: {
+    extend: {
+      colors: {
+        azul: {
+          claro: "#c5DFFF",
+          escuro: "#061E3C",
+          hover: "#1057B0",
+        },
+      },
+      fontFamily:{
+          inter: ['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+};
